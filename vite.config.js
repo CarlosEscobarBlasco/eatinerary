9514ca21a81cc6ed55f3c507f0adb27efd1da9ee
Carlos <carlos@example.com>
2026-04-17 15:07:30 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          supabase: ['@supabase/supabase-js'],
-          vue: ['vue', 'vue-router', 'pinia']
-        }
-      }
-    }
   }
 })
